498dddc7d27004103a65d223e60028cd23193677
DiegoAndress717 <diegoandress717@gmail.com>
2022-10-11 18:34:14 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">App Creada con React</p>
+      <p className="Footer-copy">
+        Todos los derechos reservados Diego Andres Salas &copy;
+      </p>
+    </div>
+  );
+};
+
+export { Footer };
